771f05f997701d065d23f4b1a191ba20517faf3c
Patrick <patrick.chapple28@icloud.com>
2024-09-02 13:50:03 -0600

    update split text nodes

--- src/mdhelper.py
+++ src/mdhelper.py
@@ -1,52 +1,23 @@
 from enums.textenum import TextType
 from textnode import TextNode
 
-'''
-class TextType(Enum):
-    TEXT = 1
-    BOLD = 2
-    ITALIC = 3
-    LINK = 4
-    CODE = 5
-    IMAGE = 6
 
-'''
 def split_nodes_delimiter(old_nodes, delimiter, text_type):
-    """
-    Split the nodes using the delimiter and return the new nodes.
-    """
     new_nodes = []
-    for node in old_nodes:
-        if node.text_type == TextType.TEXT:
-            new_nodes.extend(split_text_node(node, delimiter))
-        else:
-            new_nodes.append(node)
+    for old_node in old_nodes:
+        if old_node.text_type != TextType.TEXT:
+            new_nodes.append(old_node)
+            continue
+        split_nodes = []
+        sections = old_node.text.split(delimiter)
+        if len(sections) % 2 == 0:
+            raise ValueError("Invalid markdown, formatted section not closed")
+        for i in range(len(sections)):
+            if sections[i] == "":
+                continue
+            if i % 2 == 0:
+                split_nodes.append(TextNode(sections[i], TextType.TEXT))
+            else:
+                split_nodes.append(TextNode(sections[i], text_type))
+        new_nodes.extend(split_nodes)
     return new_nodes
-
-def split_text_node(node, delimiter):
-    """
-    Split the text node using the delimiter and return the new nodes.
-    Example: "This is text with a `code block` word"
-    delimiter = "`"
-    should find an open and closed delimiter and split the text node into three nodes. If matching delimiter is not found, raise exception.
-    return 
-    [
-        TextNode("This is text with a ", text_type_text),
-        TextNode("code block", text_type_code),
-        TextNode(" word", text_type_text),
-    ]
-    """
-    new_nodes = []
-    count = node.text.count(delimiter)
-    if count % 2 != 0:
-        raise Exception("Delimiter count is not even")
-    if count == 2:
-        open_delimiter = node.text.find(delimiter)
-        close_delimiter = node.text.find(delimiter, open_delimiter + 1)
-
-        new_nodes.append(TextNode(node.text[:open_delimiter], TextType.TEXT))
-        new_nodes.append(TextNode(node.text[open_delimiter + 1:close_delimiter], TextType.CODE))
-        new_nodes.append(TextNode(node.text[close_delimiter + 1:], TextType.TEXT))
-
-    return new_nodes
-
--- src/test_mdhelper.py
+++ src/test_mdhelper.py
@@ -10,3 +10,15 @@ class TestMdHelper(unittest.TestCase):
         node = TextNode("This is text with a `code block` word", TextType.TEXT)
         new_nodes = split_nodes_delimiter([node], "`", TextType.TEXT)
         self.assertEqual(len(new_nodes), 3)
+
+    def test_delim_bold(self):
+        node = TextNode("This is text with a **bolded** word", TextType.TEXT)
+        new_nodes = split_nodes_delimiter([node], "**", TextType.BOLD)
+        self.assertListEqual(
+            [
+                TextNode("This is text with a ", TextType.TEXT),
+                TextNode("bolded", TextType.BOLD),
+                TextNode(" word", TextType.TEXT),
+            ],
+            new_nodes,
+        )
